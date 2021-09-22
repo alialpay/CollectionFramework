e8e4d526f8d26bb2ca198911a9db62b047a8894b
alialpay <alialpay96@hotmail.com>
2021-09-23 00:16:31 +0300

    priority queue

---
+++ PriorityQueue/src/Main.java
@@ -0,0 +1,76 @@
+
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+class Player implements Comparable<Player>{
+    private String isim;
+    private int id;
+
+    public Player(String isim, int id) {
+        this.isim = isim;
+        this.id = id;
+    }
+
+    @Override
+    public int compareTo(Player player) {
+        if (this.id < player.id) {
+            return -1;
+        }
+        else if(this.id > player.id){
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" + "isim=" + isim + ", id=" + id + '}';
+    }
+    
+    
+    
+    
+    
+    
+}
+public class Main {
+    public static void main(String[] args) {
+        Queue<Player> queue = new PriorityQueue<Player>();
+        
+        queue.offer(new Player("Ali", 5));
+        queue.offer(new Player("Hazal", 1));
+        queue.offer(new Player("Betül", 100));
+        
+        while (!queue.isEmpty()) {            
+            System.out.println("Eleman Çıkarılıyor: " + queue.poll());
+        }
+        
+        
+        
+        
+        
+       /* queue.offer(5);
+        queue.offer(1);
+        queue.offer(2);
+        queue.offer(100);
+        
+       for(Integer i : queue){
+            System.out.println(i);
+        }
+        System.out.println(queue.peek());
+        
+        System.out.println(queue.contains(10));
+        
+        System.out.println("");
+        
+        while (!queue.isEmpty()) {            
+            System.out.println("Eleman Çıkarılıyor : " + queue.poll());
+        }
+       */
+       
+       
+       
+       
+       
+    }
+}
